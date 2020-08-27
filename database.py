66ff0515b383d1aeb5203fdcfef6c85a77044e55
Milton Boos Junior <junior_boos@live.com>
2020-08-27 01:42:15 +0100

    database insert docente

--- database.py
+++ database.py
@@ -4,10 +4,11 @@ conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
 
 def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
+    c.execute("CREATE TABLE IF NOT EXISTS docente(login TEXT, nome TEXT, email TEXT, emp_num REAL)")
 
 def data_entry():
-    c.execute("INSERT INTO stuffToPlot VALUES(1452549219,'2016-01-11 13:53:39','Python',6)")
+    c.execute("INSERT INTO docente VALUES('juniorboos', 'Milton Boos Junior','junior_boos@live.com',6)")
+    c.execute("INSERT INTO docente VALUES('lrmen14', 'Lucas Ribeiro Mendes','lrmen14@gmail.com',2)")
     conn.commit()
     c.close()
     conn.close()
