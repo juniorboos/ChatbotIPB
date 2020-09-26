45741a4da88583897fe2866b3eb4a2293eea97e5
Milton Boos Junior <junior_boos@live.com>
2020-09-26 09:18:53 +0100

    axis centered in image

--- mapgui.py
+++ mapgui.py
@@ -5,8 +5,11 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("Map")
 Image.MAX_IMAGE_PIXELS = 1024000000
-# root.geometry("960x720")
+# root.geometry("1000x1000")
 # root.resizable(width=FALSE, height= FALSE)
+canvas = Canvas(root, width=1000, height=1000)
+canvas.pack()
+canvas.configure(scrollregion=(-500, -500, 500, 500))
 
 floor = Image.open("Blueprint/PISO -1.png")
 # floorResized = ImageTk.PhotoImage(floor.resize((1000,1000)))
@@ -15,14 +18,19 @@ floorResized = floor.resize((1000,1000))
 locationIcon = Image.open("Blueprint/location_icon.png")
 
 # floorResized.paste(locationIcon, (500, 400), locationIcon)
-floorResized.paste(locationIcon, (710, 360), locationIcon)
+# floorResized.paste(locationIcon, (710, 360), locationIcon)
+# floorResized.paste(locationIcon, (0, 0), locationIcon)
 
 tkimage = ImageTk.PhotoImage(floorResized)
+pinIcon = ImageTk.PhotoImage(locationIcon)
 
-panel1 = Label(root, image=tkimage)
-panel1.grid(row=0, column=2, sticky=E)
-# my_label = Label(image=floorResized)
-# my_label.pack()
+
+canvas.create_image(0, 0, anchor='center', image=tkimage)
+canvas.create_image(222, -140, anchor='center', image=pinIcon)
+# canvas.create_oval(-500, -500, 500, 500, fill="red")
+
+
+# pin = Label(root, image=pinIcon).place(x=0,y=0)
 
 
 root.mainloop()
