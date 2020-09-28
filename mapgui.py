6b62feebc8e645da7ad9d9bf0a519f1afdc6ca8b
Milton Boos Junior <junior_boos@live.com>
2020-09-28 07:16:24 +0100

    can choose where the pin is

--- mapgui.py
+++ mapgui.py
@@ -6,22 +6,11 @@ import json
 
 root = Tk()
 root.title("Map")
-Image.MAX_IMAGE_PIXELS = 1024000000
-# root.geometry("1000x1000")
-# root.resizable(width=FALSE, height= FALSE)
-
-# Create canvas with size 1000x1000
-canvas = Canvas(root, width=1000, height=1000)
-canvas.pack()
-canvas.configure(scrollregion=(-500, -500, 500, 500))
-
-# Open image and resize to 1000x1000
-floor = Image.open("Blueprint/PISO -1.png")
-floorResized = floor.resize((1000,1000))
-floorResized.save('resizedfloor.png')
-
-# Set pin icon
-locationIcon = Image.open("Blueprint/location_icon.png")
+frame = Frame(root)
+frame.grid(column=0,row=0, sticky=(N,W,E,S) )
+frame.columnconfigure(0, weight = 1)
+frame.rowconfigure(0, weight = 1)
+frame.pack()
 
 choices = []
 
@@ -30,15 +19,47 @@ with open('rooms.json', encoding="utf8") as json_file:
    for floor in data["floor"]:
       for room in data["floor"][floor]:
          choices.append(room)
-         for coord in data["floor"][floor][room]:
-            floorResized.paste(locationIcon, (coord[0], coord[1]), locationIcon)
 
-print(choices)
+tkvar = StringVar(root)
+tkvar.set('Galeria de acesso') # Default
+
+popupMenu = OptionMenu(frame, tkvar, *choices)
+popupMenu.pack()
+
+
+
+Image.MAX_IMAGE_PIXELS = 1024000000
+# root.geometry("1000x1000")
+# root.resizable(width=FALSE, height= FALSE)
+
+# Create canvas with size 1000x1000
+canvas = Canvas(frame, width=1000, height=1000)
+canvas.pack()
+# canvas.configure(scrollregion=(-500, -500, 500, 500))
+
+# Open image and resize to 1000x1000
+floor = Image.open("Blueprint/PISO -1.png")
+floorResized = floor.resize((1000,1000))
+
+# Set pin icon
+locationIcon = Image.open("Blueprint/location_icon.png")
+
 tkimage = ImageTk.PhotoImage(floorResized)
 pinIcon = ImageTk.PhotoImage(locationIcon)
 
-canvas.create_image(0, 0, anchor='center', image=tkimage)
+canvas.create_image(0, 0, anchor=NW, image=tkimage)
 
+def change_dropdown(*args):
+   print( tkvar.get() )
+   selectedRoom = tkvar.get()
+   for floor in data["floor"]:
+      for room in data["floor"][floor]:
+         if (room == selectedRoom):
+            for coord in data["floor"][floor][room]:
+               canvas.create_image(coord[0], coord[1], anchor=NW, image=pinIcon)
+            break
+
+tkvar.trace('w', change_dropdown)
 
 root.mainloop()
 
